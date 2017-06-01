fcaa4fe51887d26f55643b58ab043d4e070796c3
huashang.chen <huashang.chen@wenba100.com>
2017-06-01 15:44:16 +0800

    添加css－loader

---
+++ js/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by wenba20160163 on 17/6/1.
+ */
+const a = 123;
+console.log(a);
+console.log(1);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,12 @@
 /**
  * Created by wenba20160163 on 17/6/1.
  */
+module.exports = {
+  entry: {
+    app: './js/index.js'
+  },
+  output: {
+    filename: '[name].[hash].js',
+    path: __dirname + '/dist'
+  }
+}
